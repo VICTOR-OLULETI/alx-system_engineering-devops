2720d0886fd24b84f1ac3ae67320b83577292c57
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-17 17:42:41 +0100

    create a file using puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file named 'school' in temp directory
+file { 'school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
