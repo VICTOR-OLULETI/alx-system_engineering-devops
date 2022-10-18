7dfc4e095663eeec87af0f219046b9a5c01f017c
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-18 11:01:14 +0100

    configuring file with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 file_line { 'configure file':
   ensure => present,
-  path   => '~/.ssh/authorized_keys'
+  path   => '/etc/ssh/ssh_config'
   line   => '    PasswordAuthentication no'
   match  => '#   PasswordAuthentication yes'
 }
 file_line { 'private key':
   ensure => present,
-  path   => '~/.ssh/authorized_keys'
+  path   => '/etc/ssh/ssh_config'
   line   => '    IdentityFile ~/.ssh/school'
   before => 'Port 22'
 }
