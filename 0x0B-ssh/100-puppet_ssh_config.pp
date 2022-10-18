514102d0d0d4e83d79238064fbc5d92190cade94
VICTOR-OLULETI <oluletiv@gmail.com>
2022-10-18 11:48:23 +0100

    Client configuration with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,11 +3,9 @@ file_line { 'must authenticate without password':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
-  match  => '#   PasswordAuthentication yes',
 }
 file_line { 'use the given private key':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
-  before => 'Port 22',
 }
