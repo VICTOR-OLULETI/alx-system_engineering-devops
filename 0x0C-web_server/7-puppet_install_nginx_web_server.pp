88f6e3a1458ad3da79466181ea49bec8ecd5e728
VICTOR-OLULETI <oluletiv@gmail.com>
2023-02-09 11:34:14 +0100

    configure new server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,10 +1,10 @@
 # Install nginx and configure it
 exec {'install nginx':
   command  => 'sudo apt-get -y update;
-	sudo apt-get -y upgrade; sudo apt install -y nginx; 
-	echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html;
-	sudo sed -i "s/# SSL configuration/\tlocation \/redirect_me \{\n\t\treturn 301
-	https:\/\/www.youtbue.com;\n\t\}\n\t# SSL configuration/" /etc/nginx/sites-enabled/default;
-	sudo service nginx start',
+        sudo apt-get -y upgrade; sudo apt install -y nginx;
+        echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html;
+        sudo sed -i s/"# SSL configuration"/"\tlocation \/redirect_me \{\n\t\treturn 301 \
+        https:\/\/www.youtube.com\;\n\t\}\n\t# SSL configuration"/ /etc/nginx/sites-enabled/default;
+        sudo service nginx reload',
   provider => 'shell',
 }
