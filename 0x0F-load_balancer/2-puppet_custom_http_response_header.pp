655038808c39895a26e757c5b93f10cbba98538f
VICTOR-OLULETI <oluletiv@gmail.com>
2023-02-11 07:45:17 +0100

    configure a new server

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,26 +1,46 @@
 # Installs a Nginx server with custome HTTP header
 
+#exec {'update':
+#  provider => 'shell',
+#  command  => 'sudo apt-get -y update',
+#  before   => Exec['install Nginx'],
+#}
+
+#exec {'install Nginx':
+#  provider => 'shell',
+#  command  => 'sudo apt-get -y install nginx',
+#  before   => Exec['add_header'],
+#}
+
+#exec { 'add_header':
+#  provider    => 'shell',
+#  environment => ["HOST=${HOSTNAME}"],
+#  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;
+#  /include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+#  before      => Exec['restart Nginx'],
+#}
+
+#exec { 'restart Nginx':
+#  provider => 'shell',
+#  command  => 'sudo service nginx restart',
+#}
+
 exec {'update':
-  provider => 'shell',
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
+    command  => 'sudo apt-get -y install nginx',
+    before   => Exec['install Nginx'],
 }
 
 exec {'install Nginx':
-  provider => 'shell',
-  command  => 'sudo apt-get -y install nginx',
-  before   => Exec['add_header'],
+    command  => 'sudo apt-get -y install nginx',
+    before   => Exec['add_header'],
 }
 
-exec { 'add_header':
-  provider    => 'shell',
-  environment => ["HOST=${HOSTNAME}"],
-  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;
-  /include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-  before      => Exec['restart Nginx'],
+exec {'add_header':
+    environment => ["HOST=${HOSTNAME}"],
+    command     => 'sudo sed -i "s/# SSL configuration/\tadd_header X-Served-By \$hostname;\n\t# SSL configuration/" /etc/nginx/sites-enabled/default',
+    before      => Exec['restart Nginx'],
 }
 
 exec { 'restart Nginx':
-  provider => 'shell',
-  command  => 'sudo service nginx restart',
+    command     => 'sudo service nginx restart',
 }
