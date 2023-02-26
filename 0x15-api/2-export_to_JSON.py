cac15e70825ea9e148325fd56460f050122420e8
VICTOR-OLULETI <oluletiv@gmail.com>
2023-02-26 16:00:20 +0100

    export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,37 +1,39 @@
 #!/usr/bin/python3
-""" This module uses the REST API for a given employee ID"""
-import requests
+""" This script exports data in json format """
+import csv
+import json
 import sys
+import requests
 
 
 def get_employee_todo_list(employee_id):
-    """ This function accepts the employee_id and returns
-        the employee information
-
-        Args:
-        employee_id (int): The ID of the employee.
-        
-        Returns:
-        str: A string containing the employee TODO list progress
-    """
     url = 'https://jsonplaceholder.typicode.com/todos?userId=' + employee_id
     response = requests.get(url)
     todos = response.json()
-    # count the number of completed and total tasks
+    # print(todos)
     num_total_tasks = len(todos)
     completed_tasks = [todo['title'] for todo in todos if todo['completed']]
     num_done_tasks = len(completed_tasks)
     url = 'https://jsonplaceholder.typicode.com/users/' + employee_id
     response = requests.get(url)
     employee = response.json()
-    print(employee)
     employee_name = employee['name']
-    username = employee['username']
-    userid = employee['id']
-    # print the report
+    userid = str(todos[0]['userId'])
+    usernames = employee['username']
+    dict1 = {}
+    temp_list = []
+    temp_dict = {}
+    for todo in todos:
+        temp_dict["task"] = todo['title']
+        temp_dict["completed"] = todo['completed']
+        temp_dict["username"] = usernames
+        temp_list.append(temp_dict)
+    dict1[userid] = temp_list
+
     filename = '{}.json'.format(userid)
     with open(filename, 'w') as f:
-        json.dump(todos, f)
+        json.dump(dict1, f)
+
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
