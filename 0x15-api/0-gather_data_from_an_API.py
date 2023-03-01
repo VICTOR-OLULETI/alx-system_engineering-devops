324f867e28b961bbb12f7cd3c31983d439b62221
VICTOR-OLULETI <oluletiv@gmail.com>
2023-03-01 11:36:04 +0100

    gathering data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,15 +4,16 @@ import requests
 import sys
 
 
-def get_employee_todo_list(employee_id):
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
     url = 'https://jsonplaceholder.typicode.com/'
     url_id = '{}todos?userId={}'.format(url, employee_id)
     response = requests.get(url_id)
     todos = response.json()
     num_total_tasks = len(todos)
-    completed_tasks = [todo.get('title') for todo in todos if todo.get('completed')]
+    completed_tasks = [t.get('title') for t in todos if t.get('completed')]
     num_done_tasks = len(completed_tasks)
-    url1 = '{}/users/{}'.format(url, employee_id)
+    url1 = 'https://jsonplaceholder.typicode.com/users/' + employee_id
     response = requests.get(url1)
     employee = response.json()
     employee_name = employee.get('name')
@@ -20,8 +21,3 @@ def get_employee_todo_list(employee_id):
         employee_name, num_done_tasks, num_total_tasks))
     for task_title in completed_tasks:
         print(f"\t {task_title}")
-
-
-if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    get_employee_todo_list(employee_id)
