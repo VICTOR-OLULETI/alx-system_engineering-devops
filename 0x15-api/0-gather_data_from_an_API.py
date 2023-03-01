195ce83410adf58805e8a4475982fe2c0eb05970
VICTOR-OLULETI <oluletiv@gmail.com>
2023-03-01 12:01:44 +0100

    gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,10 +7,13 @@ import sys
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
     employee_id = sys.argv[1]
-    user = 'https://jsonplaceholder.typicode.com/users/' + employee_id
+    user = 'https://jsonplaceholder.typicode.com/users/'
     res = requests.get(user)
     json_o = res.json()
-    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
+    for i in json_o:
+        if i['id'] == int(employee_id):
+            name = i['name']
+    print("Employee {} is done with tasks".format(name), end="")
 
     todos = '{}todos?userId={}'.format(url, employee_id)
     res = requests.get(todos)
