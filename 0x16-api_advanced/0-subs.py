92b89be8bf469a9983c60a23619b2e3b9c5dd976
VICTOR-OLULETI <oluletiv@gmail.com>
2023-05-31 21:52:57 +0100

    advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,7 +17,7 @@ def number_of_subscribers(subreddit):
         'User-Agent': u_agent
     }
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code != 200:
         return (0)
