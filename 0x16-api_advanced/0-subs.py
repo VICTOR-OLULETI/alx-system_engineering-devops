94b0c73a96752d4c4b27bc81197112e9fdde119d
VICTOR-OLULETI <oluletiv@gmail.com>
2023-05-31 20:45:49 +0100

    advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,5 +19,9 @@ def number_of_subscribers(subreddit):
     if response.status_code != 200:
         return (0)
     response = response.json()
+    if 'data' not in response:
+        return (0)
+    if 'subscribers' not in response.get('data'):
+        return (0)
     subscribers = response['data']['subscribers']
     return (subscribers)
