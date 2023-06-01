6899544268f664fa689189f1c0b7562e974e5c1b
VICTOR-OLULETI <oluletiv@gmail.com>
2023-06-01 21:16:20 +0100

    advanced api

---
+++ 0x16-api_advanced/100-ccount.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" This script queries Reddit API """
+import requests
+import sys
+
+
+def count_words(subreddit, word_list, after=None, counted={}, temp=0):
+    """ recurse temp, this function helps to perform the recursion
+        Variables: subreddit - chosen subreddit by user input
+                    hot_list - contains the list of the hot
+                            posts of the subreddit
+                    after - to check if there is more on another page
+    """
+    headers = {
+        'User-Agent': 'vickey'
+    }
+    params = {
+        'after': after,
+        #'count': temp,
+        'limit': 100
+    }
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(
+        url, headers=headers,
+        params=params,
+        allow_redirects=False)
+
+    if response.status_code != 200:
+        return (None)
+
+    response = response.json()
+    posts = response['data']['children']
+    # temp += response['data']['dist']
+    for post in posts:
+        title = post['data']['title']
+        for word in word_list:
+            # occurrence = title.lower().count(word)
+            occur = [i.lower() for i in title.split()]
+            occurrence = len([w for w in occur if w == word])
+            if occurrence:
+                # print(counted)
+                if counted.get(word):
+                    counted[word] = counted[word] + occurrence
+                else:
+                    counted[word] = occurrence
+    after = response['data']['after']
+    if after:
+        count_words(subreddit, word_list, after, counted, temp)
+    elif(counted == 0):
+        print("")
+        return (0)
+    else:
+        sorted_dict = dict(counted.items(), key=lambda x: x[1], reverse=True)
+        for key, value in sorted_dict.items():
+            if key not in ['key', 'reverse']:
+                print("{}: {}".format(key, value))
+    return (0)
