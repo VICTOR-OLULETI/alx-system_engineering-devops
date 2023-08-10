91ec099ab464a525aca9cce348db32ea8914b940
VICTOR-OLULETI <oluletiv@gmail.com>
2023-08-10 19:45:53 +0100

    fixes the php bug

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# debugging a wrong extension for a php file
+
+exec { 'fix-bug':
+    command => "sed -i 's/class-wp-locale\.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+    path    => '/usr/local/bin/:/bin/',
+}
